61078570f0414078e01ff1b3ef6c1a0e7207045b
Lovmong <1967471283@qq.com>
2024-05-20 15:59:17 +0800

    fix bug

--- pironman-u1-firmware/fan.cpp
+++ pironman-u1-firmware/fan.cpp
@@ -2,27 +2,57 @@
 
 void fanInit(void)
 {
-    // ledcSetup(FAN_PWM_CHANNEL, FAN_PWM_FREQ, FAN_PWM_RESOLUTION);
-    // ledcAttachPin(FAN_PIN, FAN_PWM_CHANNEL);
-
+#if FAN_DAC_MODE
     pinMode(FAN_PIN, OUTPUT);
+#else
+    pinMode(FAN_PIN, OUTPUT);
+    ledcSetup(FAN_PWM_CHANNEL, FAN_PWM_FREQ, FAN_PWM_RESOLUTION);
+    ledcAttachPin(FAN_PIN, FAN_PWM_CHANNEL);
+    ledcWrite(FAN_PWM_CHANNEL, 0);
+#endif
 }
 
 void fanSet(uint8_t power)
 {
-    uint8_t pluseWidth = 0;
-
+    uint8_t crr = 0;
     if (power > 100)
     {
         power = 100;
     }
-    pluseWidth = (uint8_t)(255 * power / 100);
-    // ledcWrite(FAN_PWM_CHANNEL, pluseWidth);
 
-    dacWrite(FAN_PIN, 0);
+#if FAN_DAC_MODE
+
+    if (power == 0)
+    {
+        fanClose();
+    }
+    else
+    {
+        crr = DAC2_START_CRR + (uint8_t)((255 - DAC2_START_CRR) * power / 100);
+        Serial.printf("set fan dac crr: %d\n", crr);
+        dacWrite(FAN_PIN, crr);
+    }
+
+#else
+
+    if (power != 0)
+    {
+        power += 20;
+    }
+    crr = (uint8_t)(255 * power / 120);
+    Serial.printf("set fan pwm crr: %d\n", crr);
+    ledcWrite(FAN_PWM_CHANNEL, crr);
+#endif
 }
 
 void fanClose()
 {
+#if FAN_DAC_MODE
+    pinMode(FAN_PIN, OUTPUT);
+    dacDisable(FAN_PIN);
+    digitalWrite(FAN_PIN, LOW);
+    // dacWrite(FAN_PIN, 0);
+#else
     ledcWrite(FAN_PWM_CHANNEL, 0);
+#endif
 }
--- pironman-u1-firmware/fan.h
+++ pironman-u1-firmware/fan.h
@@ -1,15 +1,22 @@
 #pragma once
 
+#include <Arduino.h>
 #include "esp32-hal-gpio.h"
-#include "esp32-hal-ledc.h"
 #include "esp32-hal-dac.h"
+#include "esp32-hal-ledc.h"
 /* ----------------------------------------------------------------
     FAN -> IO18
 ----------------------------------------------------------------*/
-#define FAN_PWM_FREQ 100
-#define FAN_PWM_RESOLUTION 8 // 1-16bit (8bit->0~255)
+// issues : shttps://github.com/adafruit/circuitpython/issues/7871
+
+#define FAN_DAC_MODE 0     // 0, pwm; 1, DAC
+#define DAC2_START_CRR 140 // lowest voltage 1.8v, 140 = 255*1.8/3.3
+
 #define FAN_PIN 18
-#define FAN_PWM_CHANNEL 3
+
+#define FAN_PWM_CHANNEL 4 // tim3
+#define FAN_PWM_FREQ 200
+#define FAN_PWM_RESOLUTION 8 // 1-16bit (8bit->0~255)
 
 void fanInit(void);
 void fanSet(uint8_t power);
